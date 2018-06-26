a3df0b841d58bbe36b7cf79eca460b8520c1bb73
dmytro-khilchuk <Dmytro.Khilchuk@symphony-solutions.eu>
2018-06-26 13:37:10 +0300

    merged with seo-data-migration

--- auth-demo/src/app/home/home.component.ts
+++ auth-demo/src/app/home/home.component.ts
@@ -1,9 +1,10 @@
+import { Observer } from "rxjs/Observer";
 import { AuthService } from './../services/auth.service';
 import { Component } from '@angular/core';
 import {Subject} from "rxjs/Subject";
 import 'rxjs/add/observable/fromPromise';
 import {Observable} from "rxjs/Observable";
-
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'home',
@@ -14,25 +15,57 @@ export class HomeComponent {
   public subject: Subject<string> = new Subject<string>();
   public obs: Observable<string>;
   public d: string = this.authService.data;
+  public observer: Observer<string>;
 
   constructor(
-    public authService: AuthService
+    public authService: AuthService,
+    public location: Location
   ) {
-    this.doSmth();
-    this.handle();
+
+    this.obs = Observable.create(observer => {
+      // this.observer = observer;
+      observer.next('10');
+      observer.complete();
+    });
+
+    console.info(this.obs, 's');
+    this.obs.subscribe(console.info);
+    this.obs.subscribe(console.info);
+    // this.handle();
+    // this.doSmth();
+
+
+
   }
 
-  doSmth() {
+  doSmth() { console.info('doSmth');
     const name = 'name';
 
-    setTimeout(() => {
-      this.subject.next('200');
-      this.subject.complete();
-    }, 3000);
+    // setTimeout(() => {
+    //   this.subject.next('200');
+    //   this.subject.complete();
+    // }, 3000);
+    //
+    // this.obs = new Observable(observer => {
+    //   observer.next(name);
+    //   observer.complete();
+    // });
 
     // setTimeout(() => {
-    //   this.obs = new Observable(observer => observer.next(name));
+    //   this.obs = Observable.create(observer => {
+    //     observer.next(name);
+    //     observer.complete();
+    //   });
     // }, 2000);
+
+    // this.obs = new Observable(observer => {
+      setTimeout(() => {
+        this.observer.next(name + 3);
+        this.observer.complete();
+      }, 2000);
+    // });
+
+
 
     // this.obs = new Observable(observer => {
     //   setTimeout(() => {
@@ -42,12 +75,12 @@ export class HomeComponent {
   }
 
   handle() {
-    this.subject.toPromise().then((data) => {
-      console.info(data);
-    })
+    // this.subject.toPromise().then((data) => {
+    //   console.info(data);
+    // })
 
-    console.info(this.d);
+    // console.info(this.d);
 
-    // this.obs.subscribe(data => console.info(data));
+    this.obs.subscribe(data => console.info(data));
   }
 }
