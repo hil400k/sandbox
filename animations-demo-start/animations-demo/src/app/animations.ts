6b55bb273ad36246af4f61bceb7e7aeaea454004
dmytro-khilchuk <Dmytro.Khilchuk@symphony-solutions.eu>
2018-09-11 18:05:10 +0300

    keyframes

--- animations-demo-start/animations-demo/src/app/animations.ts
+++ animations-demo-start/animations-demo/src/app/animations.ts
@@ -1,24 +1,25 @@
-import { animate, state, style, transition, trigger } from "@angular/animations";
+import { animate, keyframes, state, style, transition, trigger } from "@angular/animations";
 
 export let fade = trigger('fade', [
-  state('void', style({
-    opacity: 0
-  })),
-  transition('void <=> *', [
-    animate(2000)
-  ])
+
 ])
 
 export let slide = trigger('slide', [
-  state('void', style({
-    transform: 'translateX(-20px)'
-  })),
   transition('void => *', [
-    animate(1000)
-  ]),
-  transition('* => void', [
-    animate(100, style({
-      transform: 'translateX(-1000px)'
+    style({
+      opacity: 0,
+      transform: 'translateX(-20px)'
+    }),
+    animate(1000, style({
+      opacity: 1,
+      transform: 'translateX(0px)'
     }))
+  ]),
+  transition(':leave', [
+    animate('0.5s', keyframes([
+      style({ offset: .2, opacity: 0 }),
+      style({ offset: .7, opacity: 1 }),
+      style({ offset: 1, opacity: 0 })
+    ]))
   ])
 ])
